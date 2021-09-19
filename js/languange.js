4c5f8c6f8bf89eb5c8106138cf9a0ad6c710a8b5
Francisco Araújo <quico.calado.araujo@gmail.com>
2021-09-19 19:50:45 +0100

    Version 3.0.1 - Small bug fixed in the languange script

--- js/languange.js
+++ js/languange.js
@@ -3,12 +3,17 @@ const link = languangeButton.querySelector("a");
 let aux = "";
 let path = window.location.pathname;
 
-if (path.substr(0, 5) === "/html") {
-    path = path.replace("/html/", "");
-    aux = "/html"
+if (path.includes("/clinicaGynebe")) {
+    path = path.replace("/clinicaGynebe", "");
+    aux = "/clinicaGynebe";
 }
 
-if (path.substr(0, 3) === "en/") {
+if (path.includes("/html")) {
+    path = path.replace("/html/", "");
+    aux = aux + "/html";
+}
+
+if (path.includes("en/")) {
     path = path.replace("en", "");
     link.setAttribute('href', window.location.origin + aux + path);
 } else {
